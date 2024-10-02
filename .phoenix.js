1089a087e5f3403e7fee7e6019f4e1cae17fef1b
Seno <4535187+s-en-o@users.noreply.github.com>
2024-10-02 14:14:01 +1300

    Update .phoenix.js

--- .phoenix.js
+++ .phoenix.js
@@ -1,275 +1,339 @@
-const movementModal = (message, override = false) => {
-    window = Window.focused()
-    screen = Screen.main().flippedVisibleFrame()
-
-    Modal.build({
-        origin(modal) {
-            return {
-                x: screen.width / 2,
-                y: screen.height / 2,
-            }
-        },
-        weight: 20,
-        duration: 1,
-        appearance: 'dark',
-        icon: window.app().icon(),
-        text: override
-            ? message + ' ' + window.app().name()
-            : 'Moving ' + window.app().name() + ' to the ' + message,
-    }).show()
-}
-
+// Modifier keys
 const MOD = ['shift', 'ctrl']
-const VERBOSE = true
-const HALF_CORRECTION = 6 // Fix padding issues for half screen windows
-
-const scr = Screen.main().flippedVisibleFrame()
-// Padding Values
-const paddingTop = 8
-const paddingLeft = 8
-const paddingRight = 8
-const paddingBottom = 8
-const paddingCenter = 8
-const paddingMiddle = 8
-// Computed sizes
-const halfWidth = (scr.width - paddingLeft - paddingRight) / 2
-const halfHeight =
-    (scr.height - paddingTop - paddingBottom) / 2 + HALF_CORRECTION
-const thirdWidth = (scr.width - paddingLeft - paddingRight) / 3
-
-const windowLocations = {
-    full: {
-        y: paddingTop,
-        x: paddingLeft,
-        width: scr.width - paddingRight,
-        height: scr.height - paddingBottom,
-    },
-    left: {
-        y: paddingTop,
-        x: paddingLeft,
-        width: halfWidth - paddingCenter,
-        height: scr.height - paddingBottom,
-    },
-    right: {
-        y: paddingTop,
-        x: halfWidth + paddingLeft + paddingCenter,
-        width: halfWidth,
-        height: scr.height - paddingBottom,
-    },
-    //Corners
-    topRight: {
-        y: paddingTop,
-        x: scr.width + paddingLeft + paddingCenter,
-        width: halfWidth,
-        height: halfHeight,
-    },
-    bottomRight: {
-        y: halfHeight + paddingTop + paddingMiddle,
-        x: scr.width + paddingLeft + paddingCenter,
-        width: halfWidth,
-        height: halfHeight,
-    },
-    topLeft: {
-        y: scr.y + paddingTop,
-        x: scr.x + paddingLeft,
-        width: halfWidth - paddingCenter,
-        height: halfHeight,
-    },
-    bottomLeft: {
-        y: scr.y + halfHeight - paddingTop,
-        x: scr.x + paddingLeft,
-        width: halfWidth - paddingCenter,
-        height: halfHeight,
-    },
-    // Extra sizes
-    rightTwoThirds: {
-        y: paddingTop,
-        x: thirdWidth + paddingLeft + paddingCenter,
-        width: thirdWidth * 2,
-        height: scr.height - paddingBottom,
-    },
-    leftTwoThirds: {
-        y: scr.y + paddingTop,
-        x: scr.x + paddingLeft,
-        width: thirdWidth * 2 - paddingCenter,
-        height: scr.height - paddingBottom - paddingTop,
-    },
-    leftThird: {
-        y: paddingTop,
-        x: paddingLeft,
-        width: thirdWidth - paddingCenter,
-        height: scr.height - paddingBottom,
-    },
-    rightThird: {
-        y: paddingTop,
-        x: thirdWidth * 2 + paddingLeft + paddingCenter,
-        width: thirdWidth,
-        height: scr.height - paddingBottom,
-    },
-}
-
+const MOD2 = ['shift', 'ctrl', 'alt']
+const MOD3 = ['shift', 'ctrl', 'alt', 'cmd']
+// ====================================
+//              Padding
+// ====================================
 const padding = 8
-const setWindow = {
-    tileHalfLeft: (window, screen) => {
-        window.setTopLeft({
-            x: screen.x + padding,
-            y: screen.y + padding,
-        })
+const paddingHalf = padding / 2
+// Find current screen
+const findScreen = () => {
+    // Get main screen
+    const screenCurrent = Screen.main()
+    const screen = screenCurrent.flippedVisibleFrame()
 
-        window.setSize({
-            width: screen.width / 2 - padding,
-            height: screen.height - padding * 2,
-        })
-    },
-    tileHalfRight: (window, screen) => {
-        Phoenix.log(screen.width)
+    return { screenCurrent: screen }
+}
+// Move to next display with two monitor setup
+const moveToNextDisplay = (window) => {
+    const screens = Screen.all()
+    const screenCurrent = Screen.main()
 
-        if (screen.width > 1280) {
-            window.setTopLeft({
-                x: screen.width,
-                y: screen.y + padding,
-            })
-        } else {
-            window.setTopLeft({
-                x: screen.width / 2,
-                y: screen.y + padding,
-            })
-        }
+    const condition = (screen) =>
+        screen.identifier() !== screenCurrent.identifier()
 
-        window.setSize({
-            width: screen.width / 2 - padding,
-            height: screen.height - padding * 2,
-        })
-    },
-    tileCenter: (window, screen) => {
-        window.setSize({
-            width: screen.width / 2 - padding,
-            height: screen.height - padding * 2,
-        })
+    const nextScreen = screens.find(condition)
+    const nextScreenFlipped = nextScreen.flippedVisibleFrame()
 
-        window.setTopLeft({
-            x: screen.x + screen.width / 2 / 2,
-            y: screen.y + padding,
-        })
-    },
-    tileMaximise: (window, screen) => {
-        window.setTopLeft({
-            x: screen.x + padding,
-            y: screen.y + padding,
-        })
-
-        window.setSize({
-            width: screen.width - padding * 2,
-            height: screen.height - padding * 2,
-        })
-    },
-    nextDisplay: (window, screen) => {
-        const screenObj = screen.flippedVisibleFrame()
-
-        window.setTopLeft({
-            x: screenObj.x + padding,
-            y: screenObj.y + padding,
-        })
-
-        window.setSize({
-            width: screenObj.width - padding * 2,
-            height: screenObj.height - padding * 2,
-        })
-    },
+    window.setFrame({
+        x: nextScreenFlipped.x + padding,
+        y: nextScreenFlipped.y + padding,
+        width: nextScreenFlipped.width - padding * 2,
+        height: nextScreenFlipped.height - padding * 2,
+    })
 }
 
-/*
-====================================
-                LEFT
-====================================
-*/
+// ====================================
+// Helper to set window tile position
+// ====================================
+const tilePosition = (position) => {
+    const window = Window.focused()
+    if (!window) return
+
+    const { screenCurrent: screen } = findScreen()
+    const heightHalf = screen.height / 2 - (padding + paddingHalf)
+    const heightFull = screen.height - padding * 2
+    const YBottomHalf = screen.y + screen.height / 2 + paddingHalf
+    const widthHalf = screen.width / 2 - (padding + paddingHalf)
+    const oneThird = screen.width / 4
+    const widthOneThird = oneThird - (padding + paddingHalf)
+    const widthTwoThird = screen.width - oneThird - (padding + paddingHalf)
+
+    switch (position) {
+        case 'leftHalf':
+            window.setFrame({
+                x: screen.x + padding,
+                y: screen.y + padding,
+                width: widthHalf,
+                height: heightFull,
+            })
+            break
+
+        case 'leftTop':
+            window.setFrame({
+                x: screen.x + padding,
+                y: screen.y + padding,
+                width: widthHalf,
+                height: heightHalf,
+            })
+            break
+
+        case 'leftBottom':
+            window.setFrame({
+                x: screen.x + padding,
+                y: YBottomHalf,
+                width: widthHalf,
+                height: heightHalf,
+            })
+            break
+
+        case 'leftOneThird':
+            window.setFrame({
+                x: screen.x + padding,
+                y: screen.y + padding,
+                width: widthOneThird,
+                height: heightFull,
+            })
+            break
+
+        case 'leftTwoThird':
+            window.setFrame({
+                x: screen.x + padding,
+                y: screen.y + padding,
+                width: widthTwoThird,
+                height: heightFull,
+            })
+            break
+
+        case 'rightHalf':
+            window.setFrame({
+                x: screen.x + screen.width / 2 + paddingHalf,
+                y: screen.y + padding,
+                width: widthHalf,
+                height: heightFull,
+            })
+            break
+
+        case 'rightTop':
+            window.setFrame({
+                x: screen.x + screen.width / 2 + padding,
+                y: screen.y + padding,
+                width: widthHalf,
+                height: heightHalf,
+            })
+            break
+
+        case 'rightBottom':
+            window.setFrame({
+                x: screen.x + screen.width / 2 + padding,
+                y: YBottomHalf,
+                width: widthHalf,
+                height: heightHalf,
+            })
+            break
+
+        case 'rightOneThird':
+            window.setFrame({
+                x: screen.x + (screen.width - oneThird) + paddingHalf,
+                y: screen.y + padding,
+                width: widthOneThird,
+                height: heightFull,
+            })
+            break
+
+        case 'rightTwoThird':
+            window.setFrame({
+                x: screen.x + oneThird + paddingHalf,
+                y: screen.y + padding,
+                width: widthTwoThird,
+                height: heightFull,
+            })
+            break
+
+        case 'screenMiddle':
+            window.setFrame({
+                x: screen.x + oneThird + paddingHalf,
+                y: screen.y + padding,
+                width: screen.width / 2 - padding * 2,
+                height: heightFull,
+            })
+            break
+
+        case 'topHalf':
+            window.setFrame({
+                x: screen.x + padding,
+                y: screen.y + padding,
+                width: screen.width - padding * 2,
+                height: heightHalf,
+            })
+            break
+
+        case 'bottomHalf':
+            window.setFrame({
+                x: screen.x + padding,
+                y: YBottomHalf,
+                width: screen.width - padding * 2,
+                height: heightHalf,
+            })
+            break
+
+        case 'fullScreen':
+            window.setFrame({
+                x: screen.x + padding,
+                y: screen.y + padding,
+                width: screen.width - padding * 2,
+                height: heightFull,
+            })
+            break
+
+        case 'nextDisplay':
+            moveToNextDisplay(window)
+
+            break
+
+        default:
+            break
+    }
+}
+// ====================================
+//                 LEFT
+// ====================================
 const leftHalf = Key.on('a', MOD, () => {
-    const window = Window.focused()
-    const screen = Screen.main().flippedVisibleFrame()
-
-    setWindow.tileHalfLeft(window, screen)
+    tilePosition('leftHalf')
 })
 
-const leftTop = new Key('q', MOD, () => {
-    if (VERBOSE) {
-        movementModal('left top')
-    }
-
-    Window.focused().setFrame(windowLocations.topLeft)
+const leftTwoThird = Key.on('a', MOD2, () => {
+    tilePosition('leftTwoThird')
 })
 
-const leftBottom = new Key('z', MOD, () => {
-    if (VERBOSE) {
-        movementModal('left bottom')
-    }
-
-    Window.focused().setFrame(windowLocations.bottomLeft)
+const leftOneThird = Key.on('a', MOD3, () => {
+    tilePosition('leftOneThird')
 })
 
-const leftTwoThirds = new Key('y', MOD, () => {
-    if (VERBOSE) {
-        movementModal('left two-thirds')
-    }
-
-    Window.focused().setFrame(windowLocations.leftTwoThirds)
+const leftTop = Key.on('q', MOD, () => {
+    tilePosition('leftTop')
 })
 
-/*
-====================================
-                RIGHT
-====================================
-*/
+const leftBottom = Key.on('z', MOD, () => {
+    tilePosition('leftBottom')
+})
+
+// ====================================
+//                 RIGHT
+// ====================================
 const rightHalf = Key.on('d', MOD, () => {
-    const window = Window.focused()
-    const screens = Screen.all()
-    const currentScreen = Screen.main().flippedVisibleFrame()
-    setWindow.tileHalfRight(window, currentScreen)
+    tilePosition('rightHalf')
 })
 
-const rightTop = new Key('e', MOD, () => {
-    if (VERBOSE) {
-        movementModal('right top')
-    }
-
-    Window.focused().setFrame(windowLocations.topRight)
+const rightTwoThird = Key.on('d', MOD2, () => {
+    tilePosition('rightTwoThird')
 })
 
-const rightBottom = new Key('c', MOD, () => {
-    if (VERBOSE) {
-        movementModal('right bottom')
-    }
-
-    Window.focused().setFrame(windowLocations.bottomRight)
+const rightOneThird = Key.on('d', MOD3, () => {
+    tilePosition('rightOneThird')
 })
 
-/*
-====================================
-                ELSE
-====================================
-*/
-const centre = Key.on('s', MOD, () => {
-    const window = Window.focused()
-    const screen = Screen.main().flippedVisibleFrame()
-
-    setWindow.tileCenter(window, screen)
+const rightTop = Key.on('e', MOD, () => {
+    tilePosition('rightTop')
 })
 
-const maximise = Key.on('f', MOD, () => {
-    const window = Window.focused()
-    const screen = Screen.main().flippedVisibleFrame()
+const rightBottom = Key.on('c', MOD, () => {
+    tilePosition('rightBottom')
+})
+// ====================================
+//                 MIDDLE
+// ====================================
+const screenMiddle = Key.on('s', MOD, () => {
+    tilePosition('screenMiddle')
+})
+// ====================================
+//                 ELSE
+// ====================================
+const fullScreen = Key.on('f', MOD, () => {
+    tilePosition('fullScreen')
+})
 
-    setWindow.tileMaximise(window, screen)
+const topHalf = Key.on('w', MOD, () => {
+    tilePosition('topHalf')
+})
+
+const bottomHalf = Key.on('x', MOD, () => {
+    tilePosition('bottomHalf')
 })
 
 // Two monitor setup
-const nextDisplay = Key.on('left', MOD, () => {
-    const window = Window.focused()
-    const screens = Screen.all()
-    const currentScreen = Screen.main()
-
-    screens.forEach((screen) => {
-        if (currentScreen.identifier() !== screen.identifier()) {
-            setWindow.nextDisplay(window, screen)
-        }
-    })
+const nextDisplay = Key.on('right', MOD, () => {
+    tilePosition('nextDisplay')
 })
+
+// ====================================
+// Examples from other users
+// ====================================
+
+// Computed sizes
+// const halfWidth = (scr.width - paddingLeft - paddingRight) / 2
+// const halfHeight =
+//     (scr.height - paddingTop - paddingBottom) / 2 + HALF_CORRECTION
+// const thirdWidth = (scr.width - paddingLeft - paddingRight) / 3
+
+// const windowLocations = {
+//     full: {
+//         y: paddingTop,
+//         x: paddingLeft,
+//         width: scr.width - paddingRight,
+//         height: scr.height - paddingBottom,
+//     },
+//     left: {
+//         y: paddingTop,
+//         x: paddingLeft,
+//         width: halfWidth - paddingCenter,
+//         height: scr.height - paddingBottom,
+//     },
+//     right: {
+//         y: paddingTop,
+//         x: halfWidth + paddingLeft + paddingCenter,
+//         width: halfWidth,
+//         height: scr.height - paddingBottom,
+//     },
+//     //Corners
+//     topRight: {
+//         y: paddingTop,
+//         x: scr.width + paddingLeft + paddingCenter,
+//         width: halfWidth,
+//         height: halfHeight,
+//     },
+//     bottomRight: {
+//         y: halfHeight + paddingTop + paddingMiddle,
+//         x: scr.width + paddingLeft + paddingCenter,
+//         width: halfWidth,
+//         height: halfHeight,
+//     },
+//     topLeft: {
+//         y: scr.y + paddingTop,
+//         x: scr.x + paddingLeft,
+//         width: halfWidth - paddingCenter,
+//         height: halfHeight,
+//     },
+//     bottomLeft: {
+//         y: scr.y + halfHeight - paddingTop,
+//         x: scr.x + paddingLeft,
+//         width: halfWidth - paddingCenter,
+//         height: halfHeight,
+//     },
+//     // Extra sizes
+//     rightTwoThirds: {
+//         y: paddingTop,
+//         x: thirdWidth + paddingLeft + paddingCenter,
+//         width: thirdWidth * 2,
+//         height: scr.height - paddingBottom,
+//     },
+//     leftTwoThirds: {
+//         y: scr.y + paddingTop,
+//         x: scr.x + paddingLeft,
+//         width: thirdWidth * 2 - paddingCenter,
+//         height: scr.height - paddingBottom - paddingTop,
+//     },
+//     leftThird: {
+//         y: paddingTop,
+//         x: paddingLeft,
+//         width: thirdWidth - paddingCenter,
+//         height: scr.height - paddingBottom,
+//     },
+//     rightThird: {
+//         y: paddingTop,
+//         x: thirdWidth * 2 + paddingLeft + paddingCenter,
+//         width: thirdWidth,
+//         height: scr.height - paddingBottom,
+//     },
+// }

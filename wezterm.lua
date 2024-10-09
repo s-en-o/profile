dbd8bf382d31201b3d2190fbde15d9fc5816c383
Seno <4535187+s-en-o@users.noreply.github.com>
2024-10-09 15:28:04 +1300

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -1,76 +1,134 @@
 -- Pull in the wezterm API
-local wezterm = require("wezterm")                                                                                             
+local wezterm = require("wezterm")
+
 -- This will hold the configuration.
 local config = wezterm.config_builder()
+
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
 config.color_scheme = "Catppuccin Mocha (Gogh)"
 config.automatically_reload_config = true
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "RESIZE"
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
 config.font_size = 16.0
 
 config.window_padding = {
-        left = 3,
-        right = 3,
-        top = 3,
-        bottom = 3,
+	left = 6,
+	right = 6,
+	top = 6,
+	bottom = 6,
 }
+
 config.background = {
-        {
-                source = {
-                        Color = "#282c35",
-                },
-                width = "100%",
-                height = "100%",
-                opacity = 0.9,
-        },
+	{
+		source = {
+			Color = "#282c35",
+		},
+		width = "100%",
+		height = "100%",
+		opacity = 0.9,
+	},
 }
 -- from: https://akos.ma/blog/adopting-wezterm/
 config.hyperlink_rules = {
-        -- Matches: a URL in parens: (URL)
-        {
-                regex = "\\((\\w+://\\S+)\\)",
-                format = "$1",
-                highlight = 1,
-        },
-        -- Matches: a URL in brackets: [URL]
-        {
-                regex = "\\[(\\w+://\\S+)\\]",
-                format = "$1",
-                highlight = 1,
-        },
-        -- Matches: a URL in curly braces: {URL}
-        {
-                regex = "\\{(\\w+://\\S+)\\}",
-                format = "$1",
-                highlight = 1,
-        },
-        -- Matches: a URL in angle brackets: <URL>
-        {
-                regex = "<(\\w+://\\S+)>",
-                format = "$1",
-                highlight = 1,
-        },
-        -- Then handle URLs not wrapped in brackets
-        {
-                -- Before
-                --regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
-                --format = '$0',
-                -- After
-                regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
-                format = "$1",
-                highlight = 1,
-        },
-        -- implicit mailto link
-        {
-                regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
-                format = "mailto:$0",
-        },
+	-- Matches: a URL in parens: (URL)
+	{
+		regex = "\\((\\w+://\\S+)\\)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in brackets: [URL]
+	{
+		regex = "\\[(\\w+://\\S+)\\]",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in curly braces: {URL}
+	{
+		regex = "\\{(\\w+://\\S+)\\}",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Matches: a URL in angle brackets: <URL>
+	{
+		regex = "<(\\w+://\\S+)>",
+		format = "$1",
+		highlight = 1,
+	},
+	-- Then handle URLs not wrapped in brackets
+	{
+		-- Before
+		--regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+		--format = '$0',
+		-- After
+		regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+		format = "$1",
+		highlight = 1,
+	},
+	-- implicit mailto link
+	{
+		regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+		format = "mailto:$0",
+	},
 }
+
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+	local title = tab_info.tab_title
+	-- if the tab title is explicitly set, take that
+	if title and #title > 0 then
+		return title
+	end
+	-- Otherwise, use the title from the active pane
+	-- in that tab
+	return tab_info.active_pane.title
+end
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local edge_background = "#0b0022"
+	local background = "#1b1032"
+	local foreground = "#808080"
+
+	if tab.is_active then
+		background = "#2b2042"
+		foreground = "#c0c0c0"
+	elseif hover then
+		background = "#3b3052"
+		foreground = "#909090"
+	end
+
+	local edge_foreground = background
+
+	local title = tab_title(tab)
+
+	-- ensure that the titles fit in the available space,
+	-- and that we have room for the edges.
+	title = wezterm.truncate_right(title, max_width - 2)
+
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
+end)
+
 -- and finally, return the configuration to wezterm
 return config
